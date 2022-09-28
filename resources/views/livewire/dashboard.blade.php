132b809a017a0aecab207e06e51c531f918bfaea
Somessaran Kobishanth <sarankoby@outlook.com>
2022-09-28 21:41:16 +0530

    dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,59 +1,182 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
-
-    <button type="button" class="btn btn-primary" wire:click="oprnModel" ata-target="#exampleModal">Modal
-        With Form</button>
-        {{-- <button wire:click="oprnModel">open model</button> --}}
-
-
-    {{-- @section('model') --}}
-    <!-- Modal with form -->
-    <div wire:ignore.self class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="formModal"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="formModal">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-
-                        <div class="form-group">
-                            <label>Username</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fas fa-envelope"></i>
-                                    </div>
-                                </div>
-                                <input type="text" class="form-control" wire:model="new_name" placeholder="Email" name="email">
+{{-- <div>
+       <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <div class="card">
+            <div class="card-statistic-4">
+                <div class="align-items-center justify-content-between">
+                    <div class="row ">
+                        <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                            <div class="card-content">
+                                <h5 class="font-15"> Total Customers</h5>
+                                <h2 class="mb-3 font-16">{{ $list_data }}</h2>
+                                <p class="mb-0">‎</p>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label>Password</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fas fa-lock"></i>
-                                    </div>
-                                </div>
-                                <input type="password" class="form-control" placeholder="Password" name="password">
+                        <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
+                            <div class="banner-img">
+                                <img src="{{ asset('assets/img/banner/2.png') }}" alt="">
                             </div>
                         </div>
-                        {{ $new_name}}
-                        <div class="form-group mb-0">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" name="remember" class="custom-control-input" id="remember-me">
-                                <label class="custom-control-label" for="remember-me">Remember Me</label>
-                            </div>
-                        </div>
-                        <button type="button" class="btn btn-primary m-t-15 waves-effect">LOGIN</button>
-
+                    </div>
                 </div>
             </div>
         </div>
+        </a>
     </div>
-    {{-- @endsection --}}
+</div>  --}}
+
+
+<div class="">
+    <section class="section">
+        <div class="row">
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-statistic-4">
+                            <div class="align-items-center justify-content-between">
+                                <div class="row ">
+                                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 pr-0 pt-4">
+                                        <div class="card-content">
+                                            <h5 class="font-15">Total Customers</h5>
+                                            <h2 class="mb-3 font-16">{{ $customers }}</h2>
+                                            <p class="mb-0"></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-6 pl-0">
+                                        <div class="banner-img">
+                                            <img src="{{ asset('assets/img/banner/customers.png') }}" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+
+
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="card">
+                    <div class="card-statistic-4">
+                        <div class="align-items-center justify-content-between">
+                            <div class="row ">
+                                <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                    <div class="card-content">
+                                        <h5 class="font-15"> Total Purchases</h5>
+                                        <h2 class="mb-3 font-16">{{ number_format($purchase_invoices, 2) }}</h2>
+                                        <p class="mb-0">‎</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                    <div class="banner-img">
+                                        <img src="{{ asset('assets/img/banner/purchases.png') }}" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-statistic-4">
+                            <div class="align-items-center justify-content-between">
+                                <div class="row ">
+                                    <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                        <div class="card-content">
+                                            <h5 class="font-15">Total Branches</h5>
+                                            <h2 class="mb-3 font-16">
+                                                {{ $branch_stores }} </h2>
+                                            <p class="mb-0">‎</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                        <div class="banner-img">
+                                            <img src="{{ asset('assets/img/banner/branches.png') }}" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-statistic-4">
+                            <div class="align-items-center justify-content-between">
+                                <div class="row ">
+                                    <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                        <div class="card-content">
+                                            <h5 class="font-15">Total Expenses</h5>
+                                            <h2 class="mb-3 font-16">{{ number_format($expenses, 2) }}</h2>
+                                            <p class="mb-0">‎</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                        <div class="banner-img">
+                                            <img src="{{ asset('assets/img/banner/expenses.png') }}" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="card">
+                    <div class="card-statistic-4">
+                        <div class="align-items-center justify-content-between">
+                            <div class="row ">
+                                <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                    <div class="card-content">
+                                        <h5 class="font-15">Total Dealers</h5>
+                                        <h2 class="mb-3 font-16">{{ $dealers }}</h2>
+                                        <p class="mb-0">‎</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                    <div class="banner-img">
+                                        <img src="{{ asset('assets/img/banner/dealers.png') }}" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+
+
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="card">
+                    <div class="card-statistic-4">
+                        <div class="align-items-center justify-content-between">
+                            <div class="row ">
+                                <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                    <div class="card-content">
+                                        <h5 class="font-15">Total Shops</h5>
+                                        <h2 class="mb-3 font-16">{{ $shops }}</h2>
+
+                                        <p class="mb-0">‎</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                    <div class="banner-img">
+                                        <img src="{{ asset('assets/img/banner/1.png') }}" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+</div>
+
+</section>
 </div>
